f467abb92b95dbd28bed781db9a1e464af7b9229
Eric Shorkey <eric.shorkey@gmail.com>
2021-01-16 14:36:54 -0800

    fixed

--- utility.go
+++ utility.go
@@ -4,8 +4,7 @@ import (
 	"reflect"
 )
 
-// returns true if both left and right are samesame, but different, but still same (both are 0pointers to same type, but maybe not to same concrete object)
-// if you look closely, this is at least partly stolen from: https://github.com/stretchr/testify/blob/34c6fa2dc70986bccbbffcc6130f6920a924b075/assert/assertions.go#L359
+// returns true if both left and right are pointers to objects of the same type, regardless of their values
 func verifyBothAreSameSame(left interface{}, right interface{}) bool {
 	leftPtr, rightPtr := reflect.ValueOf(left), reflect.ValueOf(right)
 	if leftPtr.Kind() != reflect.Ptr || rightPtr.Kind() != reflect.Ptr {
